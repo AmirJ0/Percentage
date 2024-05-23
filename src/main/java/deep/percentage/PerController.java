f41d12635edba12bc23f998482775bd7de653d29
AmirJ0 <amirreza.jafarzadeh07@gmail.com>
2024-05-23 12:27:59 +0330

    Set footer and ChoiceBox

--- src/main/java/deep/percentage/PerController.java
+++ src/main/java/deep/percentage/PerController.java
@@ -1,14 +1,26 @@
 package deep.percentage;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PerController {
+public class PerController implements Initializable {
 
-    //fields
+    @FXML
     public AnchorPane mainPane;
     public VBox inputVbox;
     public HBox inputTrueMarksHbox;
@@ -21,8 +33,32 @@ public class PerController {
     public Button calculateBtn;
     public HBox footer;
     public Hyperlink githubLink;
-    public ImageView icon;
-    public ChoiceBox choiceMode;
+    public ImageView iconView;
+    public ChoiceBox<String> choiceMode;
     public Label result;
     public Label errorBox;
+
+    //Fields
+    public String[] mode = {"نمره منفی", "بدون نمره منفی"};
+    public String modeSelected;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choiceMode.getItems().addAll(mode);
+        choiceMode.setOnAction(this::modeSelect);
+    }
+
+    public void modeSelect(Event event){
+        modeSelected = choiceMode.getValue();
+    }
+
+    public void githubLinkAction(){
+        Desktop desktop = java.awt.Desktop.getDesktop();
+        try {
+            URI githubUri = new URI("https://github.com/AmirJ0");
+            desktop.browse(githubUri);
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module deep.percentage {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens deep.percentage to javafx.fxml;
